bc3cff1d706b0e3885c1003525e57001b58742c1
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2022-12-15 17:22:21 +0100

    Añadiendo test y en calculadora metodos añadidos

--- src/main/java/dawprogramacion/calculadora/calculadora.java
+++ src/main/java/dawprogramacion/calculadora/calculadora.java
@@ -1,10 +1,17 @@
 package dawprogramacion.calculadora;
 
-
-public class calculadora{
-    public int sumar(int num1, int num2){
+public class calculadora {
+    public int sumar(int num1, int num2) {
         return num1 + num2;
     }
-
+    public int restar(int num1, int num2) {
+        return num1 - num2;
+    }
+    public double multiplicar(int num1, int num2) {
+        return num1 * num2;
+    }
+    public double dividir(int num1, int num2) {
+        return num1 / num2;
+    }
 
 }
--- src/test/java/dawprogramacion/calculadora/calculadoratest.java
+++ src/test/java/dawprogramacion/calculadora/calculadoratest.java
@@ -5,19 +5,64 @@ import org.junit.Test;
 
 public class calculadoratest {
     @Test
-    public void dosSumarDosEsCuatro(){
+    public void dosSumarDosEsCuatro() {
         int expected = 4;
         int numero1 = 2, numero2 = 2;
         calculadora calc = new calculadora();
-        
-        assertEquals(expected,calc.sumar(numero1, numero2));
+
+        assertEquals(expected, calc.sumar(numero1, numero2));
     }
-    public void cincoSumarSieteEsDoce(){
+
+    @Test
+    public void cincoSumarSieteEsDoce() {
         int expected = 12;
         int numero1 = 5, numero2 = 7;
         calculadora calc = new calculadora();
-        
-        assertEquals(expected,calc.sumar(numero1, numero2));
+
+        assertEquals(expected, calc.sumar(numero1, numero2));
     }
-    
+
+    @Test
+    public void tesSumarSieteEsDiez() {
+        int expected = 10;
+        int numero1 = 3, numero2 = 7;
+        calculadora calc = new calculadora();
+
+        assertTrue("Tres mas siete debería ser 10", expected == calc.sumar(numero1, numero2));
+    }
+
+    @Test
+    public void tesSumarSieteNoEsCien() {
+        int expected = 100;
+        int numero1 = 3, numero2 = 7;
+        calculadora calc = new calculadora();
+
+        assertFalse("Tres mas siete no debería ser 100", expected == calc.sumar(numero1, numero2));
+    }
+
+    @Test
+    public void tresMultiplicarTresEsNueve() {
+        int expected = 9;
+        int numero1 = 3, numero2 = 3;
+        calculadora calc = new calculadora();
+
+        assertTrue("Tres por tres  debería ser 9", expected == calc.multiplicar(numero1, numero2));
+    }
+    @Test
+    public void nueveDividirTresEsTres(){
+        int expected = 3;
+        int numero1= 9, numero2 = 3;
+        calculadora calc = new calculadora();
+
+        assertTrue("Nueve entre tres debería ser 3", expected == calc.dividir(numero1, numero2));
+    }
+    @Test
+    public void nueveDividirTresEsCuatro(){
+        int expected = 4;
+        int numero1= 9, numero2 = 3;
+        calculadora calc = new calculadora();
+
+        assertFalse("Nueve entre tres debería ser 3", expected == calc.dividir(numero1, numero2));
+    }
+
 }
