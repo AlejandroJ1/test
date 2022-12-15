afba18ea92ebf212a120326989a3a0435e9c6ed1
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2022-12-15 17:43:17 +0100

    Tipos de resultados en notas

--- src/main/java/dawprogramacion/notas/Notas.java
+++ src/main/java/dawprogramacion/notas/Notas.java
@@ -1,6 +1,23 @@
 package dawprogramacion.notas;
 
 public class Notas {
+    public String notaATexto(double nota){
+        String notaEnTexto;
 
+        if(nota < 0 || nota > 10){
+            notaEnTexto = "La nota no puede ser menos de 0 o mayor de 10";
+        }else if(nota < 5){
+            notaEnTexto = "Suspenso";
+        }else if(nota < 7){
+            notaEnTexto = "Aprobado";
+        }else if(nota < 9){
+            notaEnTexto = "Notable";
+        }else if(nota <10){
+            notaEnTexto = "Sobresaliente";
+        }else{
+            notaEnTexto = "Matricula de honor";
+        }
+        return notaEnTexto;
+    }
     
 }
