11d46f71b66e545c77368c94b92606c1cfa54623
AlejandroJ1 <AlejandroJ1@github.com>
2022-12-16 21:32:24 +0100

    Actualizando archivo

--- src/test/java/dawprogramacion/notas/NotasTest.java
+++ src/test/java/dawprogramacion/notas/NotasTest.java
@@ -2,15 +2,25 @@ package dawprogramacion.notas;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class NotasTest {
+    Notas notas;
+
+    @BeforeEach
+    void notasTest(){
+        notas = new Notas();
+    }
+    @AfterEach
+    void borrarObjetoNotas(){
+        notas = null;
+    }
     @Test
     public void notaNegativaNoPuedeSer(){
         String expected = "La nota no puede ser menos de 0 o mayor de 10";
         double nota = -5.4;
-        Notas notas = new Notas();
-
         assertEquals(expected, notas.notaATexto(nota), "Nota negativa no puede ser evaluada");
     }
     
