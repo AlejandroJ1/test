3aa6c951a2a6dcbff65a69e451c2838c720137a7
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2022-12-16 21:00:48 +0100

    Añadiendo contenido a notasTest.java

--- src/test/java/dawprogramacion/notas/NotasTest.java
+++ src/test/java/dawprogramacion/notas/NotasTest.java
@@ -5,6 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class NotasTest {
+    @Test
+    public void notaNegativaNoPuedeSer(){
+        String expected = "La nota no puede ser menos de 0 o mayor de 10";
+        double nota = -5.4;
+        Notas notas = new Notas();
 
+        assertEquals(expected, notas.notaATexto(nota));
+    }
     
 }
